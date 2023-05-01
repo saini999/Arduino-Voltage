4f2e9c5791ccf0a126f787181b60bd51518eab23
Noor <harnoorsaini789@gmail.com>
2023-05-01 21:08:32 +0530

    added Double phase calibration and made some changes

--- src/voltage.cpp
+++ src/voltage.cpp
@@ -13,7 +13,11 @@ void voltage::begin(byte pin, byte pin2) {
 voltage::voltage(byte pin, voltagetype voltagetype){
     _pin = pin;
     _voltagetype = voltagetype;
-    _scannum = 1;
+    if(_voltagetype == DC){
+        _scannum = 1;
+    } else {
+        _scannum = 10;
+    }
     begin(_pin);
 }
 voltage::voltage(byte pin, byte pin2, voltagetype voltagetype, phase phase){
@@ -34,40 +38,49 @@ void voltage::setScan(int scannum){
 int voltage::getScan(){
     return _scannum;
 }
-void voltage::setCalibration(float calib){
-    _calib = calib;
-}
-float voltage::getCalibration(){
-    return _calib;
-}
-float voltage::getVoltage() {
-    if(_phase == SINGLE){
-        _voltage = 0;
-        for(int i = 0; i < _scannum; i++){
-            _tempvoltage = analogRead(_pin) * _calib;
-            if(_tempvoltage > _voltage){
-                _voltage = _tempvoltage;
-            }
-        }
-        return _voltage;
-    }
-    if(_phase == DOUBLE){
-        _voltage = 0; _voltage2 = 0;
-        for(int i = 0; i < _scannum; i++){
-            _tempvoltage = analogRead(_pin) * _calib;
-            _tempvoltage2 = analogRead(_pin2) * _calib;
-            if(_tempvoltage > _voltage){
-                _voltage = _tempvoltage;
-            }
-            if(_tempvoltage2 > _voltage2){
-                _voltage2 = _tempvoltage2;
-            }
-        }
-        return (_voltage + _voltage2) * 0.863; //0.863 is the constant to calculate double phase voltage
+void voltage::setCalibration(float calib, bool pinNum){
+    if(pinNum){
+        _calib1 = calib;
+    } else {
+        _calib = calib;
     }
 }
 
-float voltage::autoCalibrate(float voltage){
-    _calib = (voltage / getVoltage()) * _calib;
-    return _calib;
+float voltage::getCalibration(bool pinNum){
+    if(pinNum){
+        return _calib1;
+    } else {
+        return _calib;
+    }
+}
+float voltage::getV(byte pin){
+    float tempv, v;
+    v = 0;
+    for(int i = 0; i < _scannum; i++){
+        tempv = analogRead(_pin) * _calib;
+        if(tempv > v){
+            v = tempv;
+        }
+    }
+    return v;
+}
+
+
+float voltage::getVoltage() {
+    if(_phase == SINGLE){
+        return getV(_pin);
+    }
+    else {
+        return (getV(_pin) + getV(_pin2)) * 0.863; //0.863 is the constant to calculate double phase voltage
+    }
+}
+
+float voltage::autoCalibrate(float voltage, bool pinNum){
+    if(pinNum){
+        _calib1 = (voltage / getV(_pin)) * _calib1;
+        return _calib1;
+    } else {
+        _calib = (voltage / getV(_pin)) * _calib;
+        return _calib;
+    }
 }
--- src/voltage.h
+++ src/voltage.h
@@ -19,19 +19,20 @@ class voltage {
         voltagetype _voltagetype;
         phase _phase;
         int _scannum;
-        float _calib, _tempvoltage, _tempvoltage2, _voltage, _voltage2;
+        float _calib, _calib1;
         //Function to setup pins as input
         void begin(byte pin);
         void begin(byte pin, byte pin2);
+        float getV(byte pin);
     public:
-        voltage(byte pin, voltagetype voltagetype); // Initialize DC or AC Single Phase Voltage reading
-        voltage(byte pin, byte pin2, voltagetype voltagetype, phase phase); //Initialize AC Double Phase Voltage Reading
+        voltage(byte pin, voltagetype voltagetype = DC); // Initialize DC or AC Single Phase Voltage reading
+        voltage(byte pin, byte pin2, voltagetype voltagetype = AC, phase phase = DOUBLE); //Initialize AC Double Phase Voltage Reading
         void setScan(int scannum); //Set Scan numbers to scan voltage multiple times before getting result, helps with keeping AC voltage Reading Stable
         int getScan(); //get the scan number
-        void setCalibration(float calib); //set Calibration Value
-        float getCalibration(); // get Calibration Value
+        void setCalibration(float calib, bool pinNum = false); //set Calibration Value
+        float getCalibration(bool pinNum = false); // get Calibration Value
         float getVoltage(); //Read the Voltage
-        float autoCalibrate(float voltage); //to be called once to get the calibration Value
+        float autoCalibrate(float voltage, bool pinNum = false); //to be called once to get the calibration Value
 };
 
 #endif
